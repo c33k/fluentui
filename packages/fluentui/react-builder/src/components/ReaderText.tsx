7900c173c3a479d639d5f2ab3902dd92fb7c3f86
Rafael L. Toscano <rltoscano@gmail.com>
2020-07-29 22:24:26 +0200

    When a component is selected show its accessible role and name
    * check if Accessibility Object Model (AOM) is enabled
    * if enabled, get role and name using it
    * if not, display warning

--- packages/fluentui/react-builder/src/components/ReaderText.tsx
+++ packages/fluentui/react-builder/src/components/ReaderText.tsx
@@ -5,15 +5,34 @@ export type ReaderTextProps = {
   selector: string;
 };
 
+const AOMDisabledMessage =
+  'Accessible Object Model (AOM) feature is not enable. For more information on how to enable it, access https://wicg.github.io/aom/caniuse.html';
+
 export const ReaderText: React.FunctionComponent<ReaderTextProps> = ({ selector }) => {
   const ref = React.createRef<HTMLElement>();
   const [text, setText] = React.useState('');
+  const [AOMWarning, setAOMWarning] = React.useState(false);
 
   React.useEffect(() => {
-    if (ref.current) {
-      const t = ref.current.ownerDocument.querySelector(selector)?.textContent;
-      setText(t);
-    }
+    const getComputedAccessibleNode = async () => {
+      if (ref.current) {
+        const element = ref.current.ownerDocument.querySelector(selector);
+        let t = element?.textContent || '';
+
+        if (!!window.getComputedAccessibleNode) {
+          const accessibleNode = await window.getComputedAccessibleNode(element);
+          console.log(`name: ${accessibleNode.name} - role: ${accessibleNode.role}`, accessibleNode);
+          t = `${accessibleNode.role} ${accessibleNode.name}`;
+        } else {
+          setAOMWarning(true);
+        }
+
+        setText(t);
+      }
+    };
+
+    getComputedAccessibleNode();
+    // eslint-disable-next-line
   }, [setText, ref.current, selector]);
 
   if (!selector) {
@@ -22,7 +41,10 @@ export const ReaderText: React.FunctionComponent<ReaderTextProps> = ({ selector 
 
   return (
     <Ref innerRef={ref}>
-      <Alert warning content={text} />
+      <>
+        <Alert warning content={text} />
+        {AOMWarning && <Alert warning content={AOMDisabledMessage} />}
+      </>
     </Ref>
   );
 };
